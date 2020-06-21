e3a38a1a9e4b528eb73ab1540aa97991dfaf232f
Tiago Matinho <tiagomartinho97@gmail.com>
2020-06-21 16:47:00 +0100

    update

--- Ouri/2.pl
+++ Ouri/2.pl
@@ -2,6 +2,6 @@ jogador(P, p2) :- X is P mod 2, X = 0.
 jogador(P, p1) :- X is P mod 2, X = 1.
 
 
-valor([X,Y,_], Y) :- Y @> X.
-valor([X,Y,_], -X) :- X @> Y.
-valor([X,Y,_], 0) :- X = Y.
+valor([_,Y,_], 10) :- Y @> 24.
+valor([X,_,_], -10) :- X @> 24.
+valor([X,Y,_], 0) :- X @< 25, Y @< 25.
